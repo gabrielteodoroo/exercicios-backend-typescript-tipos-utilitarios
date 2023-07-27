132a6215e237f64b5a554331adeee3064bbc5c42
Gabriel Teodoro <biel.teodorob@gmail.com>
2023-07-27 09:51:02 -0300

    First commit

--- 01/scripts.ts
+++ 01/scripts.ts
@@ -1,11 +1,21 @@
 type Usuario = {
-    nome: string,
-    email: string,
-    cpf: string,
-    rg: string
-    dataNacimento?: string
-}
+  nome: string;
+  email: string;
+  cpf: string;
+  rg: string;
+  dataNacimento?: string;
+};
 
 const cadastrarUsuário = (info: Usuario): Usuario => {
-    return info;
-}
+  return info;
+};
+
+type UsuarioSemRg = Omit<Usuario, "rg">;
+type UsuariosCamposObrigatorios = Required<UsuarioSemRg>;
+
+const gabriel: UsuariosCamposObrigatorios = {
+  nome: "gabriel",
+  email: "gabriel@email.com",
+  cpf: "08282504564",
+  dataNacimento: "19/11/1999",
+};
